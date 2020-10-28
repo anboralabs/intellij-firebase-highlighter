46f907f7a94eb5405fade5e0450e89af163bdf94
Darwin Algarin <dalgarins@live.com>
2020-10-27 23:18:33 -0500

    Added support to change color of strings, numbers and comments.

--- src/main/java/co/anbora/labs/firebase/syntax/highlight/ColorSettings.java
+++ src/main/java/co/anbora/labs/firebase/syntax/highlight/ColorSettings.java
@@ -18,7 +18,9 @@ public class ColorSettings implements ColorSettingsPage {
     private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[]{
             new AttributesDescriptor("Keywords", Highlighter.KEY),
             new AttributesDescriptor("Permissions", Highlighter.PERMISSION),
-            new AttributesDescriptor("Path", Highlighter.PATH)
+            new AttributesDescriptor("Path and Strings", Highlighter.PATH),
+            new AttributesDescriptor("Comments", Highlighter.COMMENT),
+            new AttributesDescriptor("Numbers", Highlighter.NUMBER)
     };
 
     @Override
@@ -35,21 +37,34 @@ public class ColorSettings implements ColorSettingsPage {
     public @NotNull String getDemoText() {
         return "rules_version = '2';\n" +
                 "service cloud.firestore {\n" +
+                "  // Allow the requestor to read or delete any resource on a path under the\n" +
+                "  // user directory.\n" +
+                "  match /users/{userId}/{anyUserFile=**} {\n" +
+                "    allow read, delete: if request.auth != null && request.auth.uid == userId;\n" +
+                "  }\n" +
+                "\n" +
                 "  match /databases/{database}/documents {\n" +
-                "\n" +
+                "    // True if the user is signed in or the requested data is 'public'\n" +
                 "    function signedInOrPublic() {\n" +
-                "        return request.auth.uid != null || resource.data.visibility == 'public';\n" +
+                "      return request.auth.uid != null || resource.data.visibility == 'public';\n" +
                 "    }\n" +
-                "\n" +
-                "    match /cities/{city} {\n" +
-                "      allow read, write: if signedInOrPublic();\n" +
-                "    }\n" +
-                "\n" +
-                "    match /users/{user} {\n" +
-                "      allow read, write: if signedInOrPublic();\n" +
+                "    match /{role}/{document=**} {\n" +
+                "      allow read, write: if\n" +
+                "          request.time < timestamp.date(2020, 9, 23) && role in request.auth.token.authorities;\n" +
                 "    }\n" +
                 "  }\n" +
-                "}\n";
+                "  // Allow the requestor to create or update their own images.\n" +
+                "  // When 'request.method' == 'delete' this rule and the one matching\n" +
+                "  // any path under the user directory would both match and the `delete`\n" +
+                "  // would be permitted.\n" +
+                "\n" +
+                "  match /users/{userId}/images/{imageId} {\n" +
+                "    // Whether to permit the request depends on the logical OR of all\n" +
+                "    // matched rules. This means that even if this rule did not explicitly\n" +
+                "    // allow the 'delete' the earlier rule would have.\n" +
+                "    allow write: if request.auth != null && request.auth.uid == userId && imageId.matches('*.png');\n" +
+                "  }\n" +
+                "}";
     }
 
     @Override
