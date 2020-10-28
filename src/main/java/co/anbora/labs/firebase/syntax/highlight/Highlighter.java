457e2198692c2ddf785d0197f2c60b92f0dc77b0
Darwin Algarin <dalgarins@live.com>
2020-10-27 23:09:46 -0500

    Added highlighting for comments, numbers, strings and call functions.

--- src/main/java/co/anbora/labs/firebase/syntax/highlight/Highlighter.java
+++ src/main/java/co/anbora/labs/firebase/syntax/highlight/Highlighter.java
@@ -22,9 +22,21 @@ public class Highlighter extends SyntaxHighlighterBase {
     public static final TextAttributesKey PATH =
             createTextAttributesKey("FIREBASE_PATH", DefaultLanguageHighlighterColors.STRING);
 
+    public static final TextAttributesKey COMMENT =
+            createTextAttributesKey("COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+
+    public static final TextAttributesKey CALL_FUNCTION =
+            createTextAttributesKey("CALL_FUNCTION", DefaultLanguageHighlighterColors.FUNCTION_CALL);
+
+    public static final TextAttributesKey NUMBER =
+            createTextAttributesKey("NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+
     private static final TextAttributesKey[] KEYWORDS_KEYS = new TextAttributesKey[]{KEY};
     private static final TextAttributesKey[] PERMISSION_KEYS = new TextAttributesKey[]{PERMISSION};
     private static final TextAttributesKey[] PATH_KEYS = new TextAttributesKey[]{PATH};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] CALL_FUNCTION_KEYS = new TextAttributesKey[]{CALL_FUNCTION};
+    private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
     private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
 
     @Override
@@ -47,8 +59,15 @@ public class Highlighter extends SyntaxHighlighterBase {
             return KEYWORDS_KEYS;
         } else if (tokenType.equals(FirebaseRulesTypes.PERMISSION)) {
             return PERMISSION_KEYS;
-        } else if ((tokenType.equals(FirebaseRulesTypes.PATH_VARIABLE))) {
+        } else if (tokenType.equals(FirebaseRulesTypes.PATH_VARIABLE)
+                || tokenType.equals(FirebaseRulesTypes.STRING)) {
             return PATH_KEYS;
+        } else if (tokenType.equals(FirebaseRulesTypes.LINE_COMMENT)) {
+            return COMMENT_KEYS;
+        } else if (tokenType.equals(FirebaseRulesTypes.CALL_FUNCTION_STATEMENT)) {
+            return CALL_FUNCTION_KEYS;
+        } else if (tokenType.equals(FirebaseRulesTypes.NUMBER)) {
+            return NUMBER_KEYS;
         }
         return EMPTY_KEYS;
     }
