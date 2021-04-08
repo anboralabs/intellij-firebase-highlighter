5410166a7079f9ac885a42d13d0eef7574122e58
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2021-04-08 15:22:12 -0500

    Fixed critical issue android.

--- src/main/kotlin/co/anbora/labs/firebase/ide/formatter/FirebaseFormattingModelBuilder.kt
+++ src/main/kotlin/co/anbora/labs/firebase/ide/formatter/FirebaseFormattingModelBuilder.kt
@@ -1,10 +1,16 @@
+@file:Suppress("UnstableApiUsage")
+
 package co.anbora.labs.firebase.ide.formatter
 
-import com.intellij.formatting.*
+import com.intellij.formatting.FormattingModel
+import com.intellij.formatting.FormattingModelBuilder
+import com.intellij.formatting.FormattingModelProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.codeStyle.CodeStyleSettings
 
 class FirebaseFormattingModelBuilder: FormattingModelBuilder {
 
-    override fun createModel(formattingContext: FormattingContext): FormattingModel {
+    /*override fun createModel(formattingContext: FormattingContext): FormattingModel {
         return FormattingModelProvider.createFormattingModelForPsiFile(
             formattingContext.containingFile,
             FirebaseFormatterBlock(
@@ -12,6 +18,17 @@ class FirebaseFormattingModelBuilder: FormattingModelBuilder {
                 spacingBuilder = createSpacingBuilder(formattingContext.codeStyleSettings)
             ),
             formattingContext.codeStyleSettings
+        )
+    }*/
+
+    override fun createModel(element: PsiElement?, settings: CodeStyleSettings): FormattingModel {
+        return FormattingModelProvider.createFormattingModelForPsiFile(
+                element!!.containingFile,
+                FirebaseFormatterBlock(
+                        node = element.node,
+                        spacingBuilder = createSpacingBuilder(settings)
+                ),
+                settings
         )
     }
 }
