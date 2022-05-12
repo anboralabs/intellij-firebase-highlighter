136bea5057e2507ad4f3338411efd29c91a8ee55
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2022-05-12 08:55:30 -0500

    Fix for searching elements.

--- src/main/kotlin/co/anbora/labs/firebase/ide/inspections/FindUsagesInspection.kt
+++ src/main/kotlin/co/anbora/labs/firebase/ide/inspections/FindUsagesInspection.kt
@@ -3,12 +3,22 @@ package co.anbora.labs.firebase.ide.inspections
 import co.anbora.labs.firebase.lang.core.psi.*
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.PsiSearchHelper
 import com.intellij.psi.search.searches.ReferencesSearch
 
 class FindUsagesInspection: FireRuleLocalInspection() {
     override fun visitElement(element: FireRuleElement, holder: ProblemsHolder, isOnTheFly: Boolean) {
         if (element !is FireRuleNamedElement) return
+        val project = element.project
         val name = element.name.orEmpty()
+        val scope = element.useScope
+
+        if (scope is GlobalSearchScope) {
+            // to keep inspection/analysis time brief, bail out if 'Find Usages' will be slow
+            val searchCost = PsiSearchHelper.getInstance(project).isCheapEnoughToSearch(name, scope, null, null)
+            if (searchCost == PsiSearchHelper.SearchCostResult.TOO_MANY_OCCURRENCES) return
+        }
 
         when (element) {
             is FireRulesFunctionDef -> checkUsages(holder, element, name)
